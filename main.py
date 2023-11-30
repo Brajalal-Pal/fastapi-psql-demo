d8462ab2a59e317a4da1d6bcea318ea742f810cf
Brajalal Pal <brajalal@rediffmail.com>
2023-11-30 08:04:17 +0530

    CRUD operation enpoints added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
 from typing import Optional, List
 from database import SessionLocal
@@ -12,6 +12,7 @@ app = FastAPI(title="My First API",
               )
 
 class Item(BaseModel):
+    id: Optional[int] = None
     name: str
     description: Optional[str] = None
     price: float    
@@ -29,27 +30,51 @@ async def get_all_items():
     items = db.query(models.Item).all()
     return items
 
-@app.get("/items/{item_id}")
+
+@app.get("/items/{item_id}", response_model=Item, status_code=status.HTTP_200_OK)
 async def get_an_item(item_id: int):
-    pass
+    item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    if not item:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
+    return item
 
 
 @app.post("/items", response_model=Item, status_code=status.HTTP_201_CREATED)
 async def create_an_item(item: Item):
     new_item = models.Item(name=item.name, description=item.description, price=item.price, on_offer=item.on_offer)
+    db_item = db.query(models.Item).filter(models.Item.name == new_item.name).first()
+
+    if db_item:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Item already exists")
+    
     db.add(new_item)
     db.commit()
     db.refresh(new_item)
     return new_item    
 
-@app.put("/items/{item_id}")
-async def update_an_item(item_id: int):
-    pass
+
+@app.put("/items/{item_id}", response_model=Item, status_code=status.HTTP_200_OK)
+async def update_an_item(item_id: int, item: Item):
+    item_to_update = db.query(models.Item).filter(models.Item.id == item_id).first()
+    if not item_to_update:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
+    
+    item_to_update.name = item.name
+    item_to_update.description = item.description
+    item_to_update.price = item.price
+    item_to_update.on_offer = item.on_offer
+    
+    db.commit()
+    db.refresh(item_to_update)
+    return item_to_update
+
 
 @app.delete("/items/{item_id}")
 async def delete_an_item(item_id: int):
-    pass
-
-
-
-
+    item_to_delete = db.query(models.Item).filter(models.Item.id == item_id).first()
+    if not item_to_delete:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
+    
+    db.delete(item_to_delete)
+    db.commit()
+    return {"message": "Item deleted successfully"}
